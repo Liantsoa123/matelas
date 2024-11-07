fc1a322417e4e77d452ef91cccfbd65fcad8f158
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-11-07 11:18:39 +0300

    form update alea

--- src/main/java/com/example/matelas/controller/BlockController.java
+++ src/main/java/com/example/matelas/controller/BlockController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlockController {
@@ -22,7 +24,12 @@ public class BlockController {
     // Handle the form submission
     @PostMapping("/saveBlock")
     public String saveBlock(@ModelAttribute Block block) {
-        blockService.saveBlock(block);
+        if ( block.getId() ==0 ){
+            blockService.saveBlock(block);
+        }else {
+            System.out.println("update"+block.getId());
+            blockService.updateBlock(block.getId(), block);
+        }
         return "redirect:/block";
     }
 
@@ -34,5 +41,20 @@ public class BlockController {
         return "bolck";
     }
 
+    @GetMapping("/getAllBolck")
+    public  String allBlock (Model model ){
+        List<Block> blocks = blockService.getAllBlocks();
+        model.addAttribute("blocks" , blocks);
+        return "blocklist";
+    }
 
+    @GetMapping("/edit-block/{id}")
+    public String editBlock(@PathVariable("id") int id, Model model) {
+        Optional<Block> block = blockService.getBlockById(id);
+        if (block.isPresent()) {
+            model.addAttribute("block", block.get());
+            return "bolck";  // Redirects to the edit-block page
+        }
+        return "redirect:/all-blocks";  // Redirect back to the all-blocks page if block not found
+    }
 }
