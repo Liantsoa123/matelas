9aaa1bf0d4d67e7cfb9f8b7dbd352d537f29c66e
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-11-21 07:09:19 +0300

    change requet to get exact reste in date given

--- src/main/java/com/example/matelas/model/achat/AchatMatierePremiereRepository.java
+++ src/main/java/com/example/matelas/model/achat/AchatMatierePremiereRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface AchatMatierePremiereRepository extends JpaRepository<AchatMatierePremiere, Integer> {
-    @Query(value = "SELECT\n" +
+    @Query(value ="SELECT\n" +
             "    amp.id,\n" +
             "    amp.id AS achat_matiere_premiere,\n" +
             "    (amp.quantite - COALESCE(SUM(sm.quantite), 0)) AS quantite_reste,\n" +
@@ -18,13 +18,18 @@ public interface AchatMatierePremiereRepository extends JpaRepository<AchatMatie
             "FROM\n" +
             "    achat_matiere_premiere amp\n" +
             "        LEFT JOIN\n" +
-            "    sortie_matiere_premiere sm ON amp.id = sm.achat_matiere_premiere_id\n" +
-            "WHERE amp.date_achat <= :date\n" +
-            "  AND amp.matiere_premier_id = :matierePremiereId\n" +
-            "group by amp.date_achat, amp.quantite, amp.id\n" +
-            "HAVING  (amp.quantite - COALESCE( SUM(sm.quantite), 0)) > 0\n" +
+            "    sortie_matiere_premiere sm\n" +
+            "    ON amp.id = sm.achat_matiere_premiere_id\n" +
+            "        AND sm.date_sortie <= ?\n" +
+            "WHERE\n" +
+            "    amp.date_achat <= ?\n" +
+            "  AND amp.matiere_premier_id = ?\n" +
+            "GROUP BY\n" +
+            "    amp.date_achat, amp.quantite, amp.id\n" +
+            "HAVING\n" +
+            "    (amp.quantite - COALESCE(SUM(sm.quantite), 0)) > 0\n" +
             "ORDER BY\n" +
-            "    amp.date_achat ASC;" , nativeQuery = true)
+            "    amp.date_achat ASC;\n", nativeQuery = true)
     List<Object[]> getRestesStockWithAchatMatierePremiere(
             @Param("date") java.sql.Date date,
             @Param("matierePremiereId") int matierePremiereId);
